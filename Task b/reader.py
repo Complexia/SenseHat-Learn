67206b4b62036fcf8b428f03b82f2495189c48c3
Remi <s3399752@student.rmit.edu.au>
2020-03-28 20:41:43 +1100

    quickfix

--- Task b/reader.py
+++ Task b/reader.py
@@ -8,19 +8,21 @@ class Reader:
 		self.red = (255,0,0)
 
 	def checkComfort(self, temp):
-		
-		##try:
-		with open('config.json') as json_file:
-			data = json.load(json_file)
-			if temp < data["cold_max"]:
-				return blue
-			elif temp >= data["comfortable_min"] and temp < data["comfortable_max"]:
-				return green
-			else:
-				return red
+		blue = (0,0,255)
+		green = (0,255,0)
+		red = (255,0,0)
+		try:
+			with open('config.json') as json_file:
+				data = json.load(json_file)
+				if temp < data["cold_max"]:
+					return blue
+				elif temp >= data["comfortable_min"] and temp < data["comfortable_max"]:
+					return green
+				else:
+					return red
 
-		##except:
-			##print("Invalid Json format.")
+		except:
+			print("Invalid Json format.")
 
 
 		
