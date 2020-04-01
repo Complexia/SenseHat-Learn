b2e61796f49495111d872588f2de6ab4a9988965
Remi <s3399752@student.rmit.edu.au>
2020-04-01 23:29:23 +1100

    changed colors on emoji1

--- Task a/screens.py
+++ Task a/screens.py
@@ -1,15 +1,16 @@
 class Screens():
     @staticmethod
     def getScreenOne(c):
-        b = (0, 0, 0)
+        b = (0, 0, 204)
+        a = (255,51,51)
         return [
             c, c, c, c, c, c, c, c,
             c, b, b, c, c, b, b, c,
             c, b, b, c, c, b, b, c,
             c, c, c, c, c, c, c, c,
-            c, b, c, c, c, c, b, c,
-            c, b, b, c, c, b, b, c,
-            c, c, b, b, b, b, c, c,
+            c, a, c, c, c, c, a, c,
+            c, a, a, c, c, a, a, c,
+            c, c, a, a, a, a, c, c,
             c, c, c, c, c, c, c, c
         ]
 
